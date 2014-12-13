5ac14515c396bc6283bd8be4f51da5a0cd04344a
Nolan Price <senshuponbiki@gmail.com>
2014-12-13 13:41:54 -0800

    Adding trigger projectile and logic for it.

--- Assets/Scripts/FireProjectile.js
+++ Assets/Scripts/FireProjectile.js
@@ -12,10 +12,24 @@ function Update() {
 			clone = Instantiate(projectile, transform.position, transform.rotation);
 			
 	        clone.transform.LookAt(hit.point); 
-	        clone.rigidbody.AddForce(clone.transform.forward * speed);
+	        if (clone.rigidbody) {
+	        	clone.rigidbody.AddForce(clone.transform.forward * speed);
+        	}
 	        clone.transform.Rotate(Vector3.right * 90);
 	        
 	        Destroy(clone, 3);
    	    }
+	} else if (Input.GetMouseButtonDown(1)) {
+//		var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+//	    var hit : RaycastHit;
+//	    if (Physics.Raycast(ray, hit, 1000)){
+//	        var clone : GameObject;
+//			clone = Instantiate(projectile, transform.position, transform.rotation);
+//			
+//	        clone.transform.LookAt(hit.point); 
+//	        clone.transform.Rotate(Vector3.right * 90);
+//	        
+//	        Destroy(clone, 3);
+//   	    }
 	}
 }
--- Assets/Scripts/ProjectileController.js
+++ Assets/Scripts/ProjectileController.js
@@ -1,7 +1,20 @@
 ﻿#pragma strict
 
 var speed : float;
+var simpleForce : float;
+private var target : Vector3;
 
-function Update () {
-//	rigidbody.MovePosition(transform.forward * speed * Time.deltaTime);
+function OnTriggerEnter(other : Collider) {
+	if (other.rigidbody) {
+		other.rigidbody.AddForce(other.ClosestPointOnBounds(transform.position) * simpleForce);
+	}	
+}
+
+function Update() {
+	transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+}
+
+function Start() {
+	var ray : Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+	target = ray.GetPoint(1000);
 }
